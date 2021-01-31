38b0a3614278de77b050b00b8804ef41ef8fe322
joesimop <joesimop8@gmail.com>
2021-01-30 19:49:40 -0800

    Theme Caching!

--- themes.js
+++ themes.js
@@ -1,20 +1,36 @@
 let root = document.documentElement;
-let theme = 1;
-
-console.log("here");
+let theme;
 
 function toggleTheme(){
-  if (theme){
+  if (theme == 1){
     root.style.setProperty("--text", "#f2f2f2");
     root.style.setProperty("--bg", "#121314");
     root.style.setProperty("--accent", "#a6e0cc");
     document.getElementById("toggler").className = "fa fa-sun-o";
+    localStorage.setItem("theme", 0);
     theme = 0;
   } else{
     root.style.setProperty("--text", "#121314");
     root.style.setProperty("--bg", "#f2f2f2");
     root.style.setProperty("--accent", "#a6e0cc");
     document.getElementById("toggler").className = "fa fa-moon-o";
+    localStorage.setItem("theme", 1);
     theme = 1;
   }
+  console.log(theme);
+}
+
+function updateTheme(theme){
+  if (theme == 1){
+    root.style.setProperty("--text", "#121314");
+    root.style.setProperty("--bg", "#f2f2f2");
+    root.style.setProperty("--accent", "#a6e0cc");
+    document.getElementById("toggler").className = "fa fa-moon-o";
+  } else{
+    root.style.setProperty("--text", "#f2f2f2");
+    root.style.setProperty("--bg", "#121314");
+    root.style.setProperty("--accent", "#a6e0cc");
+    document.getElementById("toggler").className = "fa fa-sun-o";
+  }
+
 }
