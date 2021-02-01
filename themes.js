6010f9fdc8a3fff2f256ff22df7096890a669071
joesimop <joesimop8@gmail.com>
2021-01-31 23:05:57 -0800

    Clean up time!

--- themes.js
+++ themes.js
@@ -3,21 +3,14 @@ let theme;
 
 function toggleTheme(){
   if (theme == 1){
-    root.style.setProperty("--text", "#f2f2f2");
-    root.style.setProperty("--bg", "#121314");
-    root.style.setProperty("--accent", "#a6e0cc");
-    document.getElementById("toggler").className = "fa fa-sun-o";
     localStorage.setItem("theme", 0);
     theme = 0;
+    updateTheme(theme);
   } else{
-    root.style.setProperty("--text", "#121314");
-    root.style.setProperty("--bg", "#f2f2f2");
-    root.style.setProperty("--accent", "#a6e0cc");
-    document.getElementById("toggler").className = "fa fa-moon-o";
     localStorage.setItem("theme", 1);
     theme = 1;
+    updateTheme(theme);
   }
-  console.log(theme);
 }
 
 function updateTheme(theme){
